b1d96aab4ab5ac3b7dc5e8b3b0d00b9090c15bf4
Diana R <romanuk13082006@gmail.com>
2024-09-26 23:37:10 +0300

    Add all the main points

--- main.js
+++ main.js
@@ -6,3 +6,45 @@ function myFunction() {
     x.style.display = "none";
   }
 }
+
+function showPoint1() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point1").style.display = "block";
+}
+
+function showPoint2() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point2").style.display = "block";
+}
+
+function showPoint3() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point3").style.display = "block";
+}
+
+function showPoint4() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point4").style.display = "block";
+}
+
+function showPoint5() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point5").style.display = "block";
+}
+
+function showPoint6() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point6").style.display = "block";
+}
+
+function showPoint7() {
+  var points = document.querySelectorAll(".field-describe > div");
+  points.forEach((point) => (point.style.display = "none"));
+  document.getElementById("point7").style.display = "block";
+}
